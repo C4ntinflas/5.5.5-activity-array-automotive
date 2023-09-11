61094f79c4b4ebc12ca9672277819d883a878a2c
Jonathan <jonathanrodriguez069@gmail.com>
2023-09-10 21:27:18 -0400

    classes made

--- factory.js
+++ factory.js
@@ -1,3 +1,21 @@
-class Factory{
-    
+class Factory extends Vehicle{
+    constructor(location, owner){
+        super();
+        this.location = location;
+        this.owner = owner;
+        this.vehicles = [];
+    }
+    createVehicle(vehicle){
+        this.vehicles.push(vehicle)
+    }
+    listVehicles(){
+        return this.vehicles;
+    }
+    paintVehicle(vin, paintColor) {
+        for (let i = 0; i < this.vehicles.length; i++) {
+            if (this.vehicles[i].vin === vin) {
+                this.vehicles[i].paint(paintColor);
+            }
+        }
+    }
 }
--- vehicle.js
+++ vehicle.js
@@ -1,4 +1,17 @@
 class Vehicle {
-    
+    constructor(make, model, year, price, vin, color){
+    this.make = make;
+    this.model = model;
+    this.year = year
+    this.price = price;
+    this.vin = vin;
+    this.color = color;
+    }
+    drive(){
+        return this.year + ' ' + this.make + ' ' + this.model + ' ' + 'drives'
+    }
+    paint(color){
+       return this.color = color;
+    }
 }
 
